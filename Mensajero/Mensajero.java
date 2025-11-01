94db1cf80bc0704d5d50f76c7acf2fb70638eae3
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-11-01 09:51:57 -0500

    1.5

---
+++ Mensajero/Mensajero.java
@@ -0,0 +1,24 @@
+package Mensajero;
+
+public class Mensajero {
+    void enviar() {
+    }
+}
+
+class Datos extends Mensajero {
+    String correo;
+    String Asunto;
+    String Destinatario;
+
+}
+
+class SMS extends Mensajero{
+
+}
+
+class main extends Mensajero{
+    public static void main(String[] args) {
+
+    }
+
+}
